b5c59f1194353ef54471adafe7f8e3c9160618ae
kyle <kyle.marcy@gmail.com>
2019-03-23 10:44:26 -0400

    built up the footer

--- wp-content/themes/_tabula_rasa/footer.php
+++ wp-content/themes/_tabula_rasa/footer.php
@@ -1,22 +1,65 @@
 <?php 
-    $footer_nav = wp_nav_menu(array(
-        'echo' => false
-        ,'walker' => new NavWalker()
-        ,'theme_location' => 'footer'
-    ));
+/**
+ * Footer
+ */
+
+    
 
 ?>
 <footer>
+
+    <!-- big section at the top -->
+    <section>
+        <h1>Tech Law SF Group INC.</h1>
+        <h3>A california law corporation</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, vitae repellendus fugiat corrupti id adipisci ad eligendi odio vel hic.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, vitae repellendus fugiat corrupti id adipisci ad eligendi odio vel hic.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, vitae repellendus fugiat corrupti id adipisci ad eligendi odio vel hic.</p>
+    </section>
+
+    <!-- 4 column section -->
+    <section>
+        <ul>
+            <li>
+                <ul>
+                    <li><h4>Contact Us</h4></li>
+                    <li><a href="tel:415-200-2977"><i class="fa fa-phone"></i><span>415-200-2977</span></a></li>
+                    <li><a href="mailto:accounts@techlawsf.com"><i class="fa fa-envelope"></i><span>accounts@techlawsf.com</span></a></li>
+                    <li><a href="javascript:;"><i class="fa fa-map"></i><span>2090 Broadway Street, Suite 604<br>San Francisco, California 94115</span></a></li>
+                </ul>
+            </li>
+            <li>
+                <ul>
+                    <li><h4>Practice Areas</h4></li>
+                    <li><a href="javascript:;">Featured Post 1</a></li>
+                    <li><a href="javascript:;">Featured Post 2</a></li>
+                    <li><a href="javascript:;">Featured Post 3</a></li>
+                </ul>
+            </li>
+            <li>
+                <ul>
+                    <li><h4>Legal</h></li>
+                    <li><a href="javascript:;">Attorney Advertising</a></li>
+                    <li><a href="javascript:;">Privacy Policy</a></li>
+                    <li><a href="javascript:;">Terms</a></li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+    
+    <!-- tiny bar at the bottom -->
+    <div>
+        <small>&copy; Copyright 2019, Tech Law SF Group Inc.</small>
+    </div>
+
+
     <?php 
-        echo $footer_nav;
-     ?>
-</footer>
-<?php
-    // Do wp_footer
+ 
     if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
     ?> <script src="//localhost:35729/livereload.js"></script> <?php
     }
     wp_footer();
 ?>
+</footer>
 </body>
 </html>
--- wp-content/themes/_tabula_rasa/header.php
+++ wp-content/themes/_tabula_rasa/header.php
@@ -5,12 +5,10 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title><?php echo get_bloginfo('name') . ' | ' . get_bloginfo('description'); ?></title>
-		<?php wp_head(); ?>		
+        <title><?php echo get_bloginfo('name') . ' | ' . get_bloginfo('description'); ?></title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
-		<!--[if lt IE 9]>
-		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		<?php wp_head(); ?>		
 	</head>
 	<body <?php body_class(); ?>>
     <?php 
